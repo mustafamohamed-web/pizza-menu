0da0b8faa5471bf25281cf44a5a8740b28e22f3f
mustafa mohamed <mustafa@trison.uk>
2024-03-06 13:08:57 +0000

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <h1 className="heading"> - FAST REACT PIZZA CO. -</h1>
+        <h3 className="subheading">OUR MENU</h3>
+        <p className="description">
+          Authentic Italian cuisine. 6 creative dishes to choose from. All from
+          our stone oven, all organic, all delicious
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
