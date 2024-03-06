9b69ad27502edb1838fb8d5636bba3eded71faec
mustafa mohamed <mustafa@trison.uk>
2024-03-06 21:51:41 +0000

    grid items fixed

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -4,14 +4,18 @@ import PropTypes from "prop-types";
 export const MenuItem = ({ name, ingredients, photoName, price, soldOut }) => {
   return (
     <>
-      <li className="menu-item">
-        <div className="img-container">
-          <img src={`../../../public/assets/${name}.jpg`} className="img" />
-        </div>
+      <li className={`menu-item ${soldOut ? "soldOut" : ""}`}>
+        <img
+          src={`../../../public/assets/${name}.jpg`}
+          className="img"
+          alt={photoName}
+        />
+
         <div className="menu-items">
           <h3 className="name">{name}</h3>
           <p className="ingredients">{ingredients}</p>
           <p className="price">{price}</p>
+          <p>{soldOut ? "Sold Out" : ""}</p>
         </div>
       </li>
     </>
@@ -19,7 +23,9 @@ export const MenuItem = ({ name, ingredients, photoName, price, soldOut }) => {
 };
 
 MenuItem.propTypes = {
-  name: PropTypes.string.isRequired, // string is required
+  name: PropTypes.string.isRequired,
   price: PropTypes.number,
   ingredients: PropTypes.string.isRequired,
+  photoName: PropTypes.string.isRequired,
+  soldOut: PropTypes.bool,
 };
