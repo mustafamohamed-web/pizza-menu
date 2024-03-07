e4fe764167c17a6cee897badfe8ecc644cd3d55f
mustafa mohamed <mustafa@trison.uk>
2024-03-07 08:32:45 +0000

    store message is dynamic now

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
 import "./Footer.css";
 export const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  const storeMessage = () => {
+    return isOpen
+      ? "Welcome to our store"
+      : `We are currently closed. Our store open between ${openHour} and ${closeHour}`;
+  };
+
+  console.log(storeMessage);
+
   return (
     <div className="footer-container">
       <p className="footer-message">
-        We're open from 12:00 to 22:00. Come visit us or order online.
+        {storeMessage()} Come visit us or order online.
       </p>
       <button>Sign up</button>
     </div>
